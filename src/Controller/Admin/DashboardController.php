1e2bec7a6b0b805d83193621aa789297efb0abdb
Redkitty <roger.gerecke@gmail.com>
2020-05-03 21:01:12 +0200

    User admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -12,6 +12,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Menu\MenuItemInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -39,6 +40,7 @@ class DashboardController extends AbstractDashboardController
             ->setDateFormat('ddMMyyyy');
     }
 
+    /* Global Admin Menu */
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToCrud('Seiten', 'fa fa-columns', StaticSite::class);
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,30 +3,19 @@
 namespace App\Controller;
 
 
-use EasyCorp\Bundle\EasyAdminBundle\EventListener\AdminContextListener;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
 
     /**
      * @Route("/login", name="app_login")
-     * @param AuthenticationUtils $authenticationUtils
      * @return Response
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(): Response
     {
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-
 
         return $this->render(
             'security/login.html.twig'
