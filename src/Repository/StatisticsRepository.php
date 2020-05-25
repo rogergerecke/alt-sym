3f109756d882dbb869534fe72ff43f017de1b907
Redkitty <roger.gerecke@gmail.com>
2020-05-25 19:09:59 +0200

    Begin to intagrete the notice function remove bugs update php.ini

---
+++ src/Repository/StatisticsRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Statistics;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Statistics|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Statistics|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Statistics[]    findAll()
+ * @method Statistics[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class StatisticsRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Statistics::class);
+    }
+
+    // /**
+    //  * @return Statistics[] Returns an array of Statistics objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Statistics
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
